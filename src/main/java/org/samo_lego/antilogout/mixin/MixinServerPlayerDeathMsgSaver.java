2c708037bf17c52cd0319601d35977fffa419c72
Дима Ш. <dima.o.sh@proton.me>
2026-07-14 19:25:04 +0100

    Extend MC class instead of using a unique method

--- src/main/java/org/samo_lego/antilogout/mixin/MixinServerPlayerDeathMsgSaver.java
+++ src/main/java/org/samo_lego/antilogout/mixin/MixinServerPlayerDeathMsgSaver.java
@@ -1,6 +1,8 @@
 package org.samo_lego.antilogout.mixin;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -8,6 +10,7 @@ import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 import org.samo_lego.antilogout.datatracker.LogoutRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -16,15 +19,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
-public abstract class MixinServerPlayerDeathMsgSaver {
+public abstract class MixinServerPlayerDeathMsgSaver extends PlayerEntity {
 
     @Unique
     private static final int MAX_DEATH_MESSAGE_LENGTH = 256;
     @Unique
     private final ServerPlayerEntity self = (ServerPlayerEntity) (Object) this;
 
-    @Unique
-    public abstract net.minecraft.world.World getWorld();
+    public MixinServerPlayerDeathMsgSaver(World world, GameProfile profile) {
+        super(world, profile);
+    }
 
     /**
      * Injects into the player death handler to save death messages for fake/disconnected players.
